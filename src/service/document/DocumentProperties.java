96792e44f9755d5ca35acfe32484033e3bed8a13
Vlad <vladdemidik@gmail.com>
2019-05-22 04:53:49 +0300

    stubs

--- src/service/document/DocumentProperties.java
+++ src/service/document/DocumentProperties.java
@@ -1,7 +1,12 @@
 package src.service.document;
 
+import java.io.File;
+
+// класс в котором будут хранится свойства документов
 public class DocumentProperties {
 
+    public DocumentProperties(File file) {
 
+    }
 
 }
--- src/service/printer/PrinterManager.java
+++ src/service/printer/PrinterManager.java
@@ -1,15 +1,33 @@
 package src.service.printer;
 
 import org.springframework.stereotype.Service;
+import src.service.document.DocumentProperties;
 
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
+import java.io.File;
 
 @Service
 public class PrinterManager {
 
     // тут получаем принтеры
-    public PrintService[] getPrinters() {
+    private PrintService[] getPrinters() {
         return PrintServiceLookup.lookupPrintServices(null, null);
     }
+
+    // смотрим по свойствам доки и подбираем подходящие принтеры
+    public PrintService getPrinterByProperties(DocumentProperties properties) {
+        return getPrinters()[0];
+    }
+
+    // http://qaru.site/questions/587732/java-printing-program-output-to-a-physical-printer
+    // посмотри тут про печать
+    public void printFile(File file) {
+        PrintService printService = getPrinterByProperties(new DocumentProperties(file));
+
+
+
+    }
+
+
 }
